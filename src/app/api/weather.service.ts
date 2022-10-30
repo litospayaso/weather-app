fd152d0e5b65236c3e5d880292dd6d129c930ce7
Angel Hita <a.hita@talmundo.com>
2022-10-30 21:45:42 +0100

    tracking all possible errors from api

--- src/app/api/weather.service.ts
+++ src/app/api/weather.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpEvent, HttpHeaders } from '@angular/common/http';
 import { environment } from '@environments/environment';
-import { Observable } from 'rxjs';
+import { catchError, Observable, Subscriber } from 'rxjs';
 import { ApiBroadcastResponse } from '@app/interfaces/broadcast-interface';
+import { HttpErrorResponse } from '@angular/common/http';
 
 /**
  * Services for weather API
@@ -30,10 +31,25 @@ export class WeatherService {
   }
   /**
    * Send a get http request against OpenWeatherMap api.
+   * Here we can track the errors on the Api request.
    * @param {string} url for get request .
    * @returns http response
    */
   get(url: string): Observable<ApiBroadcastResponse> {
-    return this.httpClient.get(`${this.API_URL}${url}${url.includes('?')?'&':'?'}appid=${this.token}`) as unknown as Observable<ApiBroadcastResponse>;
+    return this.httpClient.get(`${this.API_URL}${url}${url.includes('?') ? '&' : '?'}appid=${this.token}`)
+    .pipe(catchError((error: any) => {
+      if(error.error.message) {
+        return new Observable((subscriber) => subscriber.error(error));
+      } else { // tracking errors if API is down
+        return new Observable((subscriber) => {
+          subscriber.error({
+            error: {
+              cod: typeof(error.status) === 'number' ? error.status : 500,
+              message: error.message || '500 error:Network issues'
+            }
+          });
+        });
+      }
+    })) as unknown as Observable<ApiBroadcastResponse>;
   }
 }
